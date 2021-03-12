db1471e7dc9d74182e0c17b4fb90b910a48ab81e
Dustin <JenyaYeskov>
2021-03-12 17:59:42 -0500

    added set interval for coby button, and some tryings with currency field

--- content.js
+++ content.js
@@ -21,7 +21,7 @@ async function copyLoadNumberToClipboard(loadNumberElement) {
 
 }
 
-async function setCopyLoadNumberButton() {
+async function addCopyLoadNumberButton() {
 
     let button = document.createElement("BUTTON");
     let buttonCaption = document.createTextNode("COPY LOAD # ");
@@ -38,7 +38,7 @@ async function setCopyLoadNumberButton() {
 
     loadNumberElement = await loadNumberElement;
 
-    button.id = "saveButton";
+    button.id = "customCopyButton";
     button.style = "margin-right: 10px;";
 
     button.appendChild(buttonCaption);
@@ -54,19 +54,13 @@ async function setCopyLoadNumberButton() {
 async function setCurrencyField() {
 
     let container = document.getElementsByClassName("styles_select_component__10SHU__control css-yk16xz-control")[1];
-
     let valueContainer = container.getElementsByClassName("styles_select_component__10SHU__value-container css-1hwfws3")[0];
 
-    // valueContainer = valueContainer[1];
-
-    // console.log((container)[0]);
-
-    valueContainer.className = "styles_select_component__10SHU__value-container styles_select_component__10SHU__value-container--has-value css-1hwfws3";
-
+    // valueContainer.className = "styles_select_component__10SHU__value-container styles_select_component__10SHU__value-container--has-value css-1hwfws3";
     valueContainer.removeChild(valueContainer.firstChild);
 
-    let value = document.createElement("div");
-    value.className = "styles_select_component__10SHU__single-value css-1uccc91-singleValue";
+    let singleValue = document.createElement("div");
+    singleValue.className = "styles_select_component__10SHU__single-value css-1uccc91-singleValue";
 
     let valueSpanContainer = document.createElement("span");
 
@@ -84,43 +78,57 @@ async function setCurrencyField() {
     valueSpanContainer.appendChild(text);
     valueSpanContainer.appendChild(innerSpan2);
 
-    value.appendChild(valueSpanContainer);
+    singleValue.appendChild(valueSpanContainer);
 
-    valueContainer.appendChild(value);
+    // valueContainer.appendChild(value);
 
-    // container[0][0][0].style = "box-sizing: content-box; width: 2px; background: 0px center; border: 0px; font-size: inherit; opacity: 1; outline: 0px; padding: 0px; color: inherit;";
-    let ss = valueContainer.getElementsByClassName("styles_select_component__10SHU__input")[0];
-    // console.log(ss);
-    ss.getElementsByTagName('input')[0].style = "box-sizing: content-box; width: 2px; background: 0px center; border: 0px; font-size: inherit; opacity: 0; outline: 0px; padding: 0px; color: inherit;";
+    // valueContainer.dispatchEvent(new Event("change", { bubbles: true }));
+    // valueContainer.dispatchEvent(new Event("blur", { bubbles: true }));
+
+    valueContainer.focus();
+
+    // document.execCommand("insertText", false, "$ United States Dollar USD");
+    document.execCommand("insertHTML", false, singleValue.innerHTML);
+
+    let inputContainer = valueContainer.getElementsByClassName("styles_select_component__10SHU__input")[0];
+    let inputElement = inputContainer.getElementsByTagName('input')[0];
+    // inputElement.style = "box-sizing: content-box; width: 2px; background: 0px center; border: 0px; font-size: inherit; opacity: 0; outline: 0px; padding: 0px; color: inherit;";
+
+    // inputElement.value = "$ United States Dollar USD";
+    // inputElement.dispatchEvent(new Event("change", { bubbles: true }));
+    // inputElement.dispatchEvent(new Event("blur", { bubbles: true }));
+
+    // inputElement.focus();
+    //
+    // document.execCommand("insertText", false, "$ United States Dollar USD");
+    //
 
     // let buffer = container.firstChild;
     valueContainer.appendChild(valueContainer.firstChild);
     // container.removeChild(container.firstChild);
 
-    let indicatorsContainer = container.lastChild;
-
-    let indicator1 = document.createElement("div");
-    indicator1.className = "styles_select_component__10SHU__indicator styles_select_component__10SHU__clear-indicator css-tlfecz-indicatorContainer";
-    indicator1.setAttribute("aria-hidden", "true");
-
-    let svg = document.createElement("svg");
-    svg.setAttribute("height", "20");
-    svg.setAttribute("width", "20");
-    svg.setAttribute("viewBox", "0 0 20 20");
-    svg.setAttribute("aria-hidden", "true");
-    svg.setAttribute("focusable", "false");
-    svg.className = "css-19bqh2r";
-
-    let path = document.createElement("path");
-    path.setAttribute("d", "M14.348 14.849c-0.469 0.469-1.229 0.469-1.697 0l-2.651-3.030-2.651 3.029c-0.469 0.469-1.229 0.469-1.697 0-0.469-0.469-0.469-1.229 0-1.697l2.758-3.15-2.759-3.152c-0.469-0.469-0.469-1.228 0-1.697s1.228-0.469 1.697 0l2.652 3.031 2.651-3.031c0.469-0.469 1.228-0.469 1.697 0s0.469 1.229 0 1.697l-2.758 3.152 2.758 3.15c0.469 0.469 0.469 1.229 0 1.698z");
-
-    svg.appendChild(path);
-    indicator1.appendChild(svg);
-
-
-    indicatorsContainer.lastChild.className = "styles_select_component__10SHU__indicator styles_select_component__10SHU__dropdown-indicator css-tlfecz-indicatorContainer";
-
-    indicatorsContainer.insertBefore(indicator1, indicatorsContainer.firstChild);
+    // let indicatorsContainer = container.lastChild;
+    //
+    // let indicator1 = document.createElement("div");
+    // indicator1.className = "styles_select_component__10SHU__indicator styles_select_component__10SHU__clear-indicator css-tlfecz-indicatorContainer";
+    // indicator1.setAttribute("aria-hidden", "true");
+    //
+    // let svg = document.createElement("svg");
+    // svg.setAttribute("height", "20");
+    // svg.setAttribute("width", "20");
+    // svg.setAttribute("viewBox", "0 0 20 20");
+    // svg.setAttribute("aria-hidden", "true");
+    // svg.setAttribute("focusable", "false");
+    // svg.className = "css-19bqh2r";
+    //
+    // let path = document.createElement("path");
+    // path.setAttribute("d", "M14.348 14.849c-0.469 0.469-1.229 0.469-1.697 0l-2.651-3.030-2.651 3.029c-0.469 0.469-1.229 0.469-1.697 0-0.469-0.469-0.469-1.229 0-1.697l2.758-3.15-2.759-3.152c-0.469-0.469-0.469-1.228 0-1.697s1.228-0.469 1.697 0l2.652 3.031 2.651-3.031c0.469-0.469 1.228-0.469 1.697 0s0.469 1.229 0 1.697l-2.758 3.152 2.758 3.15c0.469 0.469 0.469 1.229 0 1.698z");
+    //
+    // svg.appendChild(path);
+    // indicator1.appendChild(svg);
+    //
+    // indicatorsContainer.lastChild.className = "styles_select_component__10SHU__indicator styles_select_component__10SHU__dropdown-indicator css-tlfecz-indicatorContainer";
+    // indicatorsContainer.insertBefore(indicator1, indicatorsContainer.firstChild);
 
 
 }
@@ -138,10 +146,24 @@ async function setAllFields() {
 
     // setCurrencyField();
 
-    if (isCurrentURLMatch("https://cl.dispatchland.com/loads/view") ||
-        isCurrentURLMatch("https://cl.dispatchland.com/trip-monitor/travel-order")) {
+    // await addCopyLoadNumberButton();
 
-        setCopyLoadNumberButton();
+    setInterval(setCopyLoadNumberButton, 1000);
+
+}
+
+function checkUrl() {
+    return isCurrentURLMatch("https://cl.dispatchland.com/loads/view") ||
+        isCurrentURLMatch("https://cl.dispatchland.com/trip-monitor/travel-order")
+}
+
+async function setCopyLoadNumberButton() {
+
+    if (!checkForButton() && checkUrl()) {
+        await addCopyLoadNumberButton();
     }
 }
 
+function checkForButton() {
+    return document.querySelector("#customCopyButton") !== null;
+}
